df1cef95b51cf360d525fef4a25e215329092668
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-11-08 08:33:50 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chat box</h1>
+      <a
+        href="https://www.youtube.com/watch?v=jcOKU9f86XE&ab_channel=JavaScriptMastery"
+        alt="chatbox"
+      >
+        ChatBox youtube link
+      </a>
     </div>
   );
 }
