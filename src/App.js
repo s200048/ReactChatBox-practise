b84de618d6713b9303db442b5cb37b929d5da40b
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-11-10 07:18:08 +0800

    20211110 Added the pages but still do not know what is going on.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Chat box</h1>
+    <>
+      <ChatEngine
+        height="100vh"
+        projectID="372afcad-55f8-4255-bf03-3514f6947291"
+        userName="Admin"
+        userSecret="pass1234"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
       <a
         href="https://www.youtube.com/watch?v=jcOKU9f86XE&ab_channel=JavaScriptMastery"
-        alt="chatbox"
+        target="_blank"
+        rel="noreferrer"
       >
-        ChatBox youtube link
+        Youtube Link
       </a>
-    </div>
+    </>
   );
 }
 
